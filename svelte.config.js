c21f2389d474a42796466b5321855ad4ff378459
Christophe Paleyron <67900397+CryzerFranz@users.noreply.github.com>
2024-05-20 14:29:30 +0200

    change adapter pages to src because of dev container

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,17 @@
-import adapter from '@sveltejs/adapter-static'
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /**
  * @type {import('@sveltejs/kit').Config}
  */
 const config = {
   extensions: ['.svelte', '.md'],
-  preprocess: [vitePreprocess()],
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
-      pages: 'dist',
+      pages: 'src',
     }),
   },
-}
+};
 
-export default config
+export default config;
